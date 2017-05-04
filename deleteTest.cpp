9caa2e7a257238c6ab02f068e9903fd891de7823
zhuyidi <1311243087@qq.com>
2017-05-04 23:22:48 +0800

    cpp

---
+++ deleteTest.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: deleteTest.cpp
+	> Author: zhuyidi
+	> Mail: 1311243087@qq.com
+	> Created Time: Thu 09 Mar 2017 08:52:42 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    float *r = new float[10];
+
+    delete []r;
+
+    cout << "1111";
+}
